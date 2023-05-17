f8c3c98c77debb1afcbdb0437250e230dcab29c0
NicolasFierro <Nicolasfierrobernal@gmail.com>
2023-05-17 08:55:43 -0500

    Create Tupla.py

---
+++ Tupla.py
@@ -0,0 +1,10 @@
+#tupla
+#Ejercicio N.1
+numbers=(0,1,2,3,4,5,6,7,8,9,10)
+print(numbers)
+#Ejercicio N.2
+coordenadas = (10, 20, 30, 40)
+print(coordenadas)
+#Ejercicio N.3
+colores = ("rojo", "verde", "azul")
+print(colores)
