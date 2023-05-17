ee8529bd3edddde5ce40d8da15c31f7d08e7b93e
NicolasFierro <Nicolasfierrobernal@gmail.com>
2023-05-17 09:37:59 -0500

    Create Diccionarios.py

---
+++ Diccionarios/Diccionarios.py
@@ -0,0 +1,17 @@
+#Diccionario
+#Ejercicio N.1
+diccionario = {'a': 1, 'b': 2, 'c': 3}
+clave = 'b'
+valor = diccionario[clave]
+print(valor)
+#Ejercicio N.2
+diccionario = {'a': 1, 'b': 2, 'c': 3}
+clave = 'd'
+valor = 4
+diccionario[clave] = valor
+print(diccionario)
+#Ejercicio N.3
+diccionario = {'a': 1, 'b': 2, 'c': 3}
+clave = 'b'
+del diccionario[clave]
+print(diccionario)
